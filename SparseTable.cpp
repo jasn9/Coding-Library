9e0af4cccda3873b67b84b1653f7f7b491086cbb
Jatin Gupta <36321943+jasn9@users.noreply.github.com>
2020-02-23 17:31:23 +0530

    Update SparseTable.cpp

--- SparseTable.cpp
+++ SparseTable.cpp
@@ -10,13 +10,15 @@ using namespace std;
 typedef long long int ll; 
 typedef long double ld; 
  
+ll dp[200005][22];
+ 
  
 int main()
 {
 	std::ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
-	
+	memset(dp,-1,sizeof(dp));
 	ll n,q;
 	cin>>n>>q;
 	
@@ -25,23 +27,30 @@ int main()
 	for(ll i=0;i<n;i++)
 	{
 		cin>>arr[i];
+		dp[i][0] = arr[i];
 	}
-	
-	ll presum[n+1];
-	presum[0] = 0;
-	
-	for(ll i=0;i<n;i++)
+	//cout<<"iop"<<endl;
+	for(ll i=1;i<=18;i++)
 	{
-		presum[i+1] = presum[i]+arr[i];
+		for(ll j=0;j<n;j++)
+		{
+			//cout<<i<<" "<<j<<" "<<(j+(1LL<<(i-1)))<<endl;
+			if((((1LL<<(i-1))+j) <= n) and dp[j+(1LL<<(i-1))][i-1]>0 and dp[j][i-1]>0)
+			dp[j][i] = min(dp[j][i-1],dp[j+(1LL<<(i-1))][i-1]); 
+		}
 	}
-	
+	//cout<<"io"<<endl;
 	while(q--)
-	{
+	{	
 		ll a,b;
 		cin>>a>>b;
 		
-		cout<<presum[b]-presum[--a]<<endl;
+		ll v = log(b-a+1)/log(2);
+		//cout<<v<<endl;
+		cout<<min(dp[a-1][v],dp[b-1-(1LL<<v)+1][v])<<endl;	
+ 
 		
 	}
+	
 	
 }
